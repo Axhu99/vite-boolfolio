e4dd3cdf84e8498352cc0d453412046dcfc1f87c
Axhu99 <xhuliodreni@icloud.com>
2024-04-03 19:25:03 +0200

    add loader

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,17 @@ const endpoint = 'http://127.0.0.1:8000/api/projects/';
 export default {
   name: 'Boolfolio',
   components: { AppHeader, ProjectsList },
-  data: () => ({ projects: [] }),
+  data: () => ({ projects: [], isLoading: false }),
   methods: {
     fetchProjects() {
+      this.isLoading = true;
       axios.get(endpoint).then(res => {
         console.log(res.data);
-
         this.projects = res.data;
+      }).catch(err => {
+        console.error(err);
+      }).then(() => {
+        this.isLoading = false;
       })
     }
   },
@@ -26,8 +30,10 @@ export default {
   <AppHeader />
   <main class="container pt-3">
     <h1>Boolfolio</h1>
-    <ProjectsList :projects="projects" />
+    <AppLoader v-if="isLoading" />
+    <ProjectsList v-else :projects="projects" />
   </main>
+
 </template>
 
 <style></style>
