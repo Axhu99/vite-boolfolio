d7fdeedac67a886dd42f2eedec80e8a7bded5c1d
Axhu99 <xhuliodreni@icloud.com>
2024-04-03 17:42:45 +0200

    link api

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import axios from 'axios';
+const endpoint = 'http://127.0.0.1:8000/api/projects/';
+export default {
+  name: 'Boolfolio',
+  components: { AppHeader },
+  data: () => ({ projects: [] }),
+  methods: {
+    fetchProjects() {
+      axios.get(endpoint).then(res => {
+        console.log(res.data);
+
+        this.projects = res.data;
+      })
+    }
+  },
+  created() {
+    this.fetchProjects();
+  }
+};
+</script>
+
+<template>
+  <AppHeader />
+  <main class="container pt-3">
+    <h1>Boolfolio</h1>
+
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        <h4 v-text="project.title"></h4>
+      </li>
+    </ul>
+  </main>
+</template>
+
+<style></style>
